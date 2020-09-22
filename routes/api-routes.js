69df06bd7c2193a1fc7e9dbd499e47e3a776baec
Pat Mulhern <patrick.mulhern80@gmail.com>
2020-09-22 10:18:39 -0400

    add post route

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,7 +2,6 @@ const router = require("express").Router();
 const Workout = require("../models/workout.js");
 const db = require("../models");
 
-// Get route
 router.get("/api/workouts", (req, res) => {
   db.Workout.find({})
     .then(workout => {
@@ -13,6 +12,15 @@ router.get("/api/workouts", (req, res) => {
     });
 });
 
+router.post("/api/workouts", (req, res) => {
+  db.Workout.create({type: "workout"})
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
 
 
 module.exports = router;
