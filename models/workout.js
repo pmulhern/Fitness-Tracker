8aeb8cfc9a4eee6a767eb83b14be7c77e9706c1f
Pat Mulhern <patrick.mulhern80@gmail.com>
2020-09-22 12:17:18 -0400

    Updates to models

--- models/workout.js
+++ models/workout.js
@@ -13,16 +13,16 @@ const workoutSchema = new Schema(
         type: {
           type: String,
           trim: true,
-          required: "Enter an exercise type"
+          required: "Enter exercise"
         },
         name: {
           type: String,
           trim: true,
-          required: "Enter an exercise name"
+          required: "Exercise name"
         },
         duration: {
           type: Number,
-          required: "Enter an exercise duration in minutes"
+          required: "Enter duration in minutes"
         },
         weight: {
           type: Number
