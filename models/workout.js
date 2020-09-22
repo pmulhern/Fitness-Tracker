ce18262487d36445da85a1f107751deed11e6d88
Pat Mulhern <patrick.mulhern80@gmail.com>
2020-09-22 12:22:49 -0400

    Tightening up models

--- models/workout.js
+++ models/workout.js
@@ -46,13 +46,12 @@ const workoutSchema = new Schema(
   }
 );
 
-const Workout = mongoose.model("Workout", workoutSchema);
-
-module.exports = Workout;
-
 workoutSchema.virtual("totalDuration").get(function() {
   return this.exercises.reduce((total, exercise) => {
     return total + exercise.duration;
   }, 0);
 });
 
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
